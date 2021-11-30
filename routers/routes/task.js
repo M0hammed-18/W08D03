6c02a4266f79a1ed2bba9255eca7324a44ac7bef
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-11-30 20:23:37 +0300

    AddAuthenticationToRoutes

--- routers/routes/task.js
+++ routers/routes/task.js
@@ -6,10 +6,11 @@ const taskRouter = express.Router();
 
 taskRouter.get("/tasks", authentication,authorization,gettask)
 taskRouter.post("/task",createtask)
-taskRouter.delete("/del/:id",DelTask)
-taskRouter.get("/task/:id",TaskbyId)
-taskRouter.delete("/delete/:id",deleteTask)
-taskRouter.put("/editTask/:id",updateTask)
+
+taskRouter.delete("/del/:id",authentication,DelTask)
+taskRouter.get("/task/:id",authentication,TaskbyId)
+taskRouter.delete("/delete/:id",authentication,deleteTask)
+taskRouter.put("/editTask/:id",authentication,updateTask)
 
 
 module.exports=taskRouter;
