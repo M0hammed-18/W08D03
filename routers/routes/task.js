40ff7877f6d4b0917052b9c7f05ea42414eb2c85
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-11-30 19:59:09 +0300

    UpdateTaskRoutesDone

--- routers/routes/task.js
+++ routers/routes/task.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {gettask,createtask,DelTask,TaskbyId,deleteTask} = require("./../controllers/task")
+const {gettask,createtask,DelTask,TaskbyId,deleteTask,updateTask} = require("./../controllers/task")
 const authentication=require("./../middleware/authentication")
 const authorization=require("./../middleware/authorization")
 const taskRouter = express.Router();
@@ -9,6 +9,7 @@ taskRouter.post("/task",createtask)
 taskRouter.delete("/del/:id",DelTask)
 taskRouter.get("/task/:id",TaskbyId)
 taskRouter.delete("/delete/:id",deleteTask)
+taskRouter.put("/editTask/:id",updateTask)
 
 
 module.exports=taskRouter;
