7ec7e6a28e2f76857e5f54b57bb77839d556dae2
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-11-30 21:22:21 +0300

    indexFinsh

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express= require("express");
-const dotenv=require("dotenv");
-require("./db/index")
+const express = require("express");
+const dotenv = require("dotenv");
+require("./db/index");
 
 dotenv.config();
 
-const app=express();
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-const roleRouter=require("./routers/routes/role")
-app.use(roleRouter)
+const roleRouter = require("./routers/routes/role");
+app.use(roleRouter);
 
-const userRouter=require("./routers/routes/user")
-app.use(userRouter)
+const userRouter = require("./routers/routes/user");
+app.use(userRouter);
 
-const taskRouter=require("./routers/routes/task")
-app.use(taskRouter)
+const taskRouter = require("./routers/routes/task");
+app.use(taskRouter);
 
-const PORT=process.env.PORT;
-app.listen(PORT,()=>{
-    console.log(`server on ${PORT}`);
-})
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`server on ${PORT}`);
+});
